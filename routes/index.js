9961a44ea57a43b2124e0246b7f7e1c8abb6c2de
Francesc Muñoz <francesc19@gmail.com>
2015-07-13 20:05:17 +0200

    Ejercicio P2P obligatorio (Mod. 6)
    Rama créditos con lo solicitado en el ejercicio.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Get author page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Francesc Muñoz' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
